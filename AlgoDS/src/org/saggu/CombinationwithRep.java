64a7b30fd722f3aba771108ae1969dc8731f71a2
Randeep Singh <randeepsaggu@gmail.com>
2018-08-12 14:51:41 +0530

    adding combination with reps

---
+++ AlgoDS/src/org/saggu/CombinationwithRep.java
@@ -0,0 +1,28 @@
+package org.saggu;
+
+public class CombinationwithRep {
+	
+	static int range[] = {1,2};
+	static int limit = 2;
+	
+	public static void main(String args[]) {
+		int index = 0;
+		int start = 0;
+		int[] result = new int[limit];
+		solve(result,index,start);
+	}
+	public static void solve(int[] result, int index,int start) {
+		if(index==limit) {
+			for(int i = 0;i<limit;i++) {
+				System.out.print(result[i]);
+			}
+			System.out.println("");
+			return;
+		}
+		
+		for(int i = start;i<range.length;i++) {
+			result[index] = range[i];
+			solve(result,index+1,i);
+		}
+	}
+}
