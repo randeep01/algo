abd85acaaf92221b7df1b4c78fdaf22e5063d8f9
Randeep Singh <randeepsaggu@gmail.com>
2018-08-12 12:37:47 +0530

    added permutation with repetition

---
+++ AlgoDS/src/org/saggu/PermutationWithReps.java
@@ -0,0 +1,25 @@
+package org.saggu;
+
+public class PermutationWithReps {
+
+	static char[] range = { 'a', 'b', 'c' };
+	static int limit = 3;
+
+	public static void main(String[] args) {
+
+		int index = 0;
+		char[] result = new char[limit];
+		solve(result, index);
+	}
+
+	static void solve(char[] result, int index) {
+		if (index == limit) {
+			System.out.println(new String(result));
+			return;
+		}
+		for (int i = 0; i < range.length; i++) {
+			result[index] = range[i];
+			solve(result, index+1);
+		}
+	}
+}
